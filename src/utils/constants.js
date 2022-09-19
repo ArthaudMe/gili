0423a9579c0404ffe5185ea522ac558fd0e6b44e
Stanley Hayford <hayfordstanley@gmail.com>
2022-09-19 21:58:29 +0000

    Change server url to production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-const BASE_SERVER_URL = 'http://localhost:50001/api/v1/vendor';
+// const BASE_SERVER_URL = 'http://localhost:8000/api/v1/';
+const BASE_SERVER_URL = 'https://giliclubbackend.herokuapp.com/api/v1';
 const GIGI_CLUB_THEME_VARIANT = 'GIGI_CLUB_THEME_VARIANT';
 const GIGI_CLUB_TOKEN = 'GIGI_CLUB_TOKEN';
 const GIGI_CLUB_AUTH_DATA = 'GIGI_CLUB_AUTH_DATA';
